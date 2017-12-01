71d7d6133279b4563567379d7fe2887f39991c32
Florian Paul Schmidt <mista.tapas@gmx.net>
2017-12-01 09:20:19 +0100

    small adaptations

--- test.jl
+++ test.jl
@@ -3,16 +3,17 @@ import WAV
 
 guitar_clean = WAV.wavread("guitar_clean.wav")
 guitar_processed = WAV.wavread("guitar_processed.wav")
+guitar_short = WAV.wavread("guitar_short.wav")
 
-window_size = 100
+window_size = 10
 
 m = AmpProfiling.create_model(window_size)
 
 tm = AmpProfiling.train_model(m, guitar_clean[1][:,1], guitar_processed[1][:,1], 32, 10)
 
 moutput = (Float64)[]
-for index in 1:size(guitar_clean[1],1)
-    append!(moutput, Flux.Tracker.value(tm(guitar_clean[1][index:index+(window_size - 1)])))
+for index in 1:(size(guitar_short[1], 1) - window_size)
+    append!(moutput, Flux.Tracker.value(tm(guitar_short[1][index:(index + (window_size - 1))])))
 end
 
 WAV.wavwrite(moutput, "tm_guitar.wav", Fs=48000)
