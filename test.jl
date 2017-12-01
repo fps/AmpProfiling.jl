bb023b20635f95ecf2dabd9d299ce98fff2c2c38
Florian Paul Schmidt <mista.tapas@gmx.net>
2017-12-01 09:22:14 +0100

    do some more work

--- test.jl
+++ test.jl
@@ -5,11 +5,11 @@ guitar_clean = WAV.wavread("guitar_clean.wav")
 guitar_processed = WAV.wavread("guitar_processed.wav")
 guitar_short = WAV.wavread("guitar_short.wav")
 
-window_size = 10
+window_size = 32
 
 m = AmpProfiling.create_model(window_size)
 
-tm = AmpProfiling.train_model(m, guitar_clean[1][:,1], guitar_processed[1][:,1], 32, 10)
+tm = AmpProfiling.train_model(m, guitar_clean[1][:,1], guitar_processed[1][:,1], 32, 1000)
 
 moutput = (Float64)[]
 for index in 1:(size(guitar_short[1], 1) - window_size)
