f59072688dbeee48c07d647aab8b28fa4d8ec9fe
Florian Paul Schmidt <mista.tapas@gmx.net>
2017-12-26 17:31:33 +0100

    use the whole training set eventually

--- test_h.jl
+++ test_h.jl
@@ -3,31 +3,38 @@ include("AmpProfiling.jl")
 import WAV
 
 #function test()
-    input_range = 1:2500
-    training_input = WAV.wavread("guitar_short.wav")[1][input_range,1]
+    training_input = WAV.wavread("guitar_clean.wav")[1]
     println("# of training input samples: $(length(training_input))")
     
-    training_output = WAV.wavread("guitar_processed.wav")[1][input_range,1]
+    training_output = WAV.wavread("guitar_processed.wav")[1]
     println("# of training output samples: $(length(training_output))")
     
     test_input = WAV.wavread("guitar_short.wav")[1][:,1]
     println("# of test samples: $(length(test_input))")
     
-    nlms = 64
+    nlms = 32
     lms = 512
+    
+    batchsize = 1500
+
+    N = min(length(training_input), length(training_output)) - (batchsize + nlms + lms -1)
     
     h = AmpProfiling.H(nlms, lms)
 
-    unrolled = AmpProfiling.unroll(training_input, h, training_output)
-     
     loss(x, y) = Flux.mse(h(x), y)
     #opt = Flux.SGD(AmpProfiling.params(h), decay = 0.01)
     opt = Flux.ADAM(AmpProfiling.params(h))
     
-    for index in 1:500
+    epochs = 100
+    
+    for index in 1:epochs
+        sindex = randperm(N)[1]
+        eindex = sindex + batchsize
+        unrolled = AmpProfiling.unroll(training_input[sindex:eindex], h, training_output[sindex:eindex])
+     
         p = Permutations.array(Permutations.RandomPermutation(length(unrolled)))
         Flux.train!(loss, unrolled[p], opt)
-        println(loss(unrolled[100][1], unrolled[100][2]))
+        println(loss(unrolled[1][1], unrolled[1][2]))
     end
 #    println("Applying model to test data...")
 #    test_xs = AmpProfiling.createWindowedSamples(test_input, window_size)
